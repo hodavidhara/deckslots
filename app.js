b223b78bb8a29ddb0a6df7e388dc8d4556e68ce1
David Hara <hakuhen@gmail.com>
2015-03-11 21:56:28 -0500

    wow such fancy 404

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var koa = require('koa');
 var handlebars = require('koa-handlebars');
+var router = require('koa-router');
 
 var app = koa();
 
@@ -11,10 +12,28 @@ app.use(handlebars({
     defaultLayout: 'main'
 }));
 
-app.use(function *() {
+// Register router
+app.use(router(app));
+
+app.get('/', function *() {
     yield this.render('index', {
         text: 'Hello, World!'
     });
 });
 
+app.use(function *pageNotFound(next){
+    yield next;
+
+    if (404 != this.status) {
+        return;
+    }
+
+
+    // we need to explicitly set 404 here
+    // so that koa doesn't assign 200 on body=
+    this.status = 404;
+    yield this.render('404');
+});
+
 app.listen(3000);
+console.log('listening on port 3000');
