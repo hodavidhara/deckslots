b8776c440db9362c370699c71fcf1079ad62c507
David Hara <hakuhen@gmail.com>
2015-07-03 18:33:44 -0500

    writing some tests

--- test/service/MongoConnectionTest.js
+++ test/service/MongoConnectionTest.js
@@ -1,10 +1,49 @@
-var expect = require('chai').expect,
-    MongoConnection = require('../../src/service/MongoConnection');
+var MongoConnection = require('../../src/service/MongoConnection'),
+    MongoClient = require('mongodb').MongoClient;
 
 describe('MongoConnection', function() {
-    describe('#get()', function(){
+    beforeEach(function () {
+        MongoConnection.db = null;
+    });
+
+    describe('#get()', function() {
         it('gets a mongo connection', function () {
             return expect(MongoConnection.get()).to.eventually.be.ok
+        });
+
+        it('does not try to connect if it already has a connection', function (done) {
+            var connectSpy = sinon.spy(MongoClient, 'connect');
+            MongoConnection.get().then(function () {
+                expect(MongoConnection.db).to.be.ok;
+                expect(connectSpy).to.have.been.calledOnce;
+                return;
+            }).then(function () {
+                MongoConnection.get().then(function () {
+                    expect(MongoConnection.db).to.be.ok;
+                    expect(connectSpy).to.have.been.calledOnce;
+
+                    connectSpy.restore();
+                    done();
+                });
+            });
+        });
+
+        it('reconnects to mongo if it loses its connection', function (done) {
+            var connectSpy = sinon.spy(MongoClient, 'connect');
+            MongoConnection.get().then(function () {
+                expect(MongoConnection.db).to.be.ok;
+                expect(connectSpy).to.have.been.calledOnce;
+                MongoConnection.db = null;
+                return;
+            }).then(function () {
+                MongoConnection.get().then(function () {
+                    expect(MongoConnection.db).to.be.ok;
+                    expect(connectSpy).to.have.been.calledTwice;
+
+                    connectSpy.restore();
+                    done();
+                });
+            });
         });
     });
 });
--- test/testConfig.js
+++ test/testConfig.js
@@ -1,4 +1,10 @@
 var chai = require("chai");
+var sinon = require("sinon");
 var chaiAsPromised = require("chai-as-promised");
+var sinonChai = require("sinon-chai");
 
 chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+global.expect = chai.expect;
+global.sinon = sinon;
